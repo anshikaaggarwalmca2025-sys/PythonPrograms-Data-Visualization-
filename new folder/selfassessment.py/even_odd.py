f5c37a7f6ca73dd483440fd11e92c80ea57ea683
Anshika Aggarwal <anshika.aggarwal.mca.2025@miet.ac.in>
2026-02-24 18:38:05 +0530

    Delete new folder directory

--- new folder/selfassessment.py/even_odd.py
+++
@@ -1,9 +0,0 @@
-# Program to check whether a number is even or odd
-
-num = int(input("Enter a number: "))
-
-# If remainder when divided by 2 is 0, it is even
-if num % 2 == 0:
-    print("Even number")
-else:
-    print("Odd number")
