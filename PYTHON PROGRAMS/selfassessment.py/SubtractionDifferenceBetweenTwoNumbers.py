841c0b7bf54b08d059dc30c724464f90bd3a72a8
Anshika Aggarwal <anshika.aggarwal.mca.2025@miet.ac.in>
2026-02-24 18:35:35 +0530

    Delete PYTHON PROGRAMS/selfassessment.py directory

--- PYTHON PROGRAMS/selfassessment.py/SubtractionDifferenceBetweenTwoNumbers.py
+++
@@ -1,6 +0,0 @@
-#Subtraction – Difference Between Two Numbers
-a = 150
-b = 85
-difference = a - b
-
-print("Difference:", difference)
--- PYTHON PROGRAMS/selfassessment.py/division.py
+++
@@ -1,5 +0,0 @@
-total_marks = 450
-subjects = 5
-average = total_marks / subjects
-
-print("Average Marks:", average)
