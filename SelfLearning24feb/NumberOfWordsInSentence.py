d0d0dbbb993daf7870ca9fba0f7ab8e5403353c1
Anshika Aggarwal <anshika.aggarwal.mca.2025@miet.ac.in>
2026-02-24 16:12:55 +0530

    Add files via upload

---
+++ SelfLearning24feb/NumberOfWordsInSentence.py
@@ -0,0 +1,19 @@
+# Problem 88: Count number of words in a sentence
+
+# Read sentence
+sentence = input("Enter a sentence: ")
+
+# Split sentence into words
+words = sentence.split()
+
+# Count number of words
+count = len(words)
+
+# Print word count
+print("Number of words:", count)
+
+
+
+#output:
+Enter a sentence: Python is very easy
+Number of words: 4
