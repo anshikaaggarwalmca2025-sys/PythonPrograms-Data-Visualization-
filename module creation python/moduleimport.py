9c53e37c0b80a3dd3e2409ee51f6f119cc96e80c
Anshika Aggarwal <anshika.aggarwal.mca.2025@miet.ac.in>
2026-05-07 19:58:57 +0530

    Delete module creation python/moduleimport.py

--- module creation python/moduleimport.py
+++
@@ -1,9 +0,0 @@
-#to import the module
-from numericalcalculation import *
-
-#to display addition of two numbers
-displayaddition(10, 5)
-#to display difference of two numbers
-displaydifference(25, 45)
-#to display product of two numbers
-displayproduct(15,20)
