94758fb0b4da21e0dc98b1fbfc379872d8c1cf9c
Anshika Aggarwal <anshika.aggarwal.mca.2025@miet.ac.in>
2026-05-07 22:00:45 +0530

    Delete SelfAssessments/SelectionStatementSelfassessment/SelectionStatementSelfassessment/votingeligibility.py

--- SelfAssessments/SelectionStatementSelfassessment/SelectionStatementSelfassessment/votingeligibility.py
+++
@@ -1,9 +0,0 @@
-# Program to check voting eligibility
-
-age = int(input("Enter your age: "))
-
-# If age is 18 or more, eligible to vote
-if age >= 18:
-    print("Eligible to vote")
-else:
-    print("Not eligible to vote")
