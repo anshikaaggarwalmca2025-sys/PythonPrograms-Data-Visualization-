d32bd3616e4141cb274733879468bd687458d001
Anshika Aggarwal <anshika.aggarwal.mca.2025@miet.ac.in>
2026-05-07 19:53:34 +0530

    Delete 3d module creation/cone.py

--- 3d module creation/cone.py
+++
@@ -1,9 +0,0 @@
-#to import the module
-import numeric3dcalculation
-#to display volume of a cone
-radius = float(input("Enter the radius of the cone: "))
-height = float(input("Enter the height of the cone: "))
-volume, total_surface_area, curved_surface_area = numeric3dcalculation.conecalculator(radius, height)
-print("Volume of the cone: ", volume)
-print("Total surface area of the cone: ", total_surface_area)
-print("Curved surface area of the cone: ", curved_surface_area)
