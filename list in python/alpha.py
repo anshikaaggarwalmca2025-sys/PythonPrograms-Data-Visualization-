7ec1a2e81a5e570cd34b4277a2c78cc7d7b15979
Anshika Aggarwal <anshika.aggarwal.mca.2025@miet.ac.in>
2026-05-07 20:47:52 +0530

    Delete list in python/alpha.py

--- list in python/alpha.py
+++
@@ -1,8 +0,0 @@
-sentence = input("Enter a sentence: ")  
-#to count the number of alphabets
-count = 0
-for x in sentence:
-    #to check x is alphabet or not
-    if x.isalpha():
-        count += 1
-print("Number of alphabets in the sentence:", count)
