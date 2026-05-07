3395d3ac85965114944be1e3e501c5d260c9c8ef
Anshika Aggarwal <anshika.aggarwal.mca.2025@miet.ac.in>
2026-05-07 19:21:49 +0530

    Add files via upload

---
+++ Labwork/Python/module creation python/moduleimport.py
@@ -0,0 +1,9 @@
+#to import the module
+from numericalcalculation import *
+
+#to display addition of two numbers
+displayaddition(10, 5)
+#to display difference of two numbers
+displaydifference(25, 45)
+#to display product of two numbers
+displayproduct(15,20)
