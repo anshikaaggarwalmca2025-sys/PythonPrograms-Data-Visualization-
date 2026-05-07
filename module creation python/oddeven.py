5d3ed76d87cdb64b8a2fab43a2557ae08e5c7b54
Anshika Aggarwal <anshika.aggarwal.mca.2025@miet.ac.in>
2026-05-07 19:58:40 +0530

    Delete module creation python/oddeven.py

--- module creation python/oddeven.py
+++
@@ -1,6 +0,0 @@
-from numericcalculation import calculatorremainder
-num = int(input("Enter a number: "))
-if calculatorremainder(num,2) == 0:
-    print(num, "is an even number.")
-else:   
-     print(num, "is an odd number.")
