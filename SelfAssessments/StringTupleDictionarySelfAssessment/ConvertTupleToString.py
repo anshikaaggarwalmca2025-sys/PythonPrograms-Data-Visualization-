1c624b46aea21ef70b37c6a0467d97da9f8458c1
Anshika Aggarwal <anshika.aggarwal.mca.2025@miet.ac.in>
2026-05-07 22:01:09 +0530

    Delete SelfAssessments/StringTupleDictionarySelfAssessment/ConvertTupleToString.py

--- SelfAssessments/StringTupleDictionarySelfAssessment/ConvertTupleToString.py
+++
@@ -1,8 +0,0 @@
-# Tuple of characters
-char_tuple = ('P', 'Y', 'T', 'H', 'O', 'N')
-
-# Convert tuple to string
-result = "".join(char_tuple)
-
-print("Tuple:", char_tuple)
-print("Converted String:", result)
